4a4475deecddb1bb7477c0a373458b02112a4bd3
LuciaBeccaro <92233159+LuciaBeccaro@users.noreply.github.com>
2023-02-22 11:24:07 +0100

    Add files via upload

---
+++ my/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Unbounded&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div id="numero">
+            +3779944835
+            <a href="#" class="fa fa-instagram"></a>
+        </div>
+        <div id="login">
+            <a href="">LOGIN</a>
+        </div>
+    </header>
+    <ul id="nav">
+        <li><a href="">LOGO</a></li>
+        <li><a href="">HOMEPAGE</a></li>
+        <li><a href="">VIDEO</a></li>
+        <li><a href="">ABITUDINI</a></li>
+        <li><a href="">OBIETTIVI</a></li>
+    </ul>
+    <div id="foto">
+        <img src="./img/motivation.jpg" alt="foto Motivazione" id="fotoM">
+        <p id="pFoto">SEGUI I SOGNI!</p>
+        <button id="btn">INIZIA</button>
+    </div>
+    <div id="azzurro">
+        
+    </div>
+    <div id="cosa">
+        <div id="primo">
+            <h3>WHAT IS IT?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste totam voluptas nihil sint in corrupti magni, error hic distinctio nam, et, magnam exercitationem explicabo itaque aliquam. Accusantium id tempora culpa.</p>
+        </div>
+        <div id="foto1">
+        </div>
+        <!--TESTO SOPRA LA FOTO-->
+        <!--<div id="testo">
+            <ul>
+                <li>LAVORO</li>
+                <li>SACRIFICIO</li>
+                <li>PASSIONE</li>
+                <li>SUCCESSO</li>
+            </ul>
+            </div>-->
+        </div>
+</div>
+<footer>
+    <div id="divFooter">
+        <div>
+            Lucia Beccaro
+        </div>
+        <div>
+            DOVE
+        </div>
+        <div>
+            QUANDO
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
